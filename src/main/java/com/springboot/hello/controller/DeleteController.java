a003020b91f114cad77c9e5dfd53d819bc5198f6
gittestsite1 <gittestsite1@gmail.com>
2026-03-08 14:52:27 +0900

    RequestParam에 대한 파라미터 처리방식

--- src/main/java/com/springboot/hello/controller/DeleteController.java
+++ src/main/java/com/springboot/hello/controller/DeleteController.java
@@ -1,9 +1,6 @@
 package com.springboot.hello.controller;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/delete-api")
@@ -17,5 +14,9 @@ public class DeleteController {
         return variable;
     }
 
+    @DeleteMapping(value = "/request1")
+    public String getRequestParam(@RequestParam String email){
+        return email;
+    }
 
 }
